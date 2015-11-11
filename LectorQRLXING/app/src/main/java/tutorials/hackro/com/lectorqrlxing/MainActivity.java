ec499e70e04232f67f1ecf60052263b9132f885f
David Hackro <hackro91@gmail.com>
2015-11-10 22:25:30 -0600

    @DavidHackro

---
+++ LectorQRLXING/app/src/main/java/tutorials/hackro/com/lectorqrlxing/MainActivity.java
@@ -0,0 +1,48 @@
+package tutorials.hackro.com.lectorqrlxing;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.util.Hashtable;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ImageView imageView = (ImageView) findViewById(R.id.qrCode);
+        Bitmap bitmap = encodeToQrCode("URL", 100, 100);
+        imageView.setImageBitmap(bitmap);
+    }
+
+    public static Bitmap encodeToQrCode(String text, int width, int height){
+        QRCodeWriter writer = new QRCodeWriter();
+        BitMatrix matrix = null;
+        try {
+            matrix = writer.encode(text, BarcodeFormat.QR_CODE, 100, 100);
+        } catch (WriterException ex) {
+            ex.printStackTrace();
+        }
+        Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565);
+        for (int x = 0; x < width; x++){
+            for (int y = 0; y < height; y++){
+                bmp.setPixel(x, y, matrix.get(x,y) ? Color.BLACK : Color.WHITE);
+            }
+        }
+        return bmp;
+    }
+
+}
