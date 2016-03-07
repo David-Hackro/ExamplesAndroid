5d3afe7e63a532ecdfb0a06ea0433c28a8b948de
David Hackro <hackro91@gmail.com>
2016-03-07 12:29:56 -0600

    @DavidHackro

--- Google Maps/MyApplication/app/src/main/java/com/example/david/googlemapsix/MyItem.java
+++ Google Maps/MyApplication/app/src/main/java/com/example/david/googlemapsix/MyItem.java
@@ -1,7 +1,19 @@
 package com.example.david.googlemapsix;
 
+import com.google.android.gms.maps.model.LatLng;
+
 /**
  * Created by david on 7/03/16.
  */
-public class MyItem {
+public class MyItem implements ClusterItem {
+    private final LatLng mPosition;
+
+    public MyItem(double lat, double lng) {
+        mPosition = new LatLng(lat, lng);
+    }
+
+    @Override
+    public LatLng getPosition() {
+        return mPosition;
+    }
 }
