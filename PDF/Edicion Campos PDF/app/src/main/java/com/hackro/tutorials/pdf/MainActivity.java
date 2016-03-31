8cf7503e43f1b88bcb94431d6ff0496f63d7acd6
David Hackro <hackro91@gmail.com>
2016-03-31 14:17:59 -0600

    @DavidHackro

---
+++ PDF/Edicion Campos PDF/app/src/main/java/com/hackro/tutorials/pdf/MainActivity.java
@@ -0,0 +1,40 @@
+package com.hackro.tutorials.pdf;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.cete.dynamicpdf.merger.MergeDocument;
+import com.cete.dynamicpdf.merger.PdfDocument;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        try {
+
+            InputStream inputStream = this.getAssets().open("DavidHackro.pdf");
+            long avail = 0;
+            avail = inputStream.available();
+
+            byte[] samplePDF = new byte[(int) avail];
+            inputStream.read(samplePDF , 0, (int) avail);
+            inputStream.close();
+            PdfDocument objPDF = new PdfDocument(samplePDF);
+
+            MergeDocument document = new MergeDocument(objPDF);
+            document.getForm().getFields().getFormField("nombre").setValue("Tutoriales Hackro");
+            document.getForm().getFields().getFormField("twitter").setValue("@DavidHackro");
+            document.draw("/sdcard/FilledPDF.pdf");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
